7a50e17a0c969cef5969179cf33878b6e103eab0
HazemUsama <hazemosama681@gmail.com>
2023-08-24 22:35:54 +0300

    maybe this is the problem

--- helper.c
+++ helper.c
@@ -14,7 +14,9 @@ int countTokens(char *str, const char *delim)
 {
 	int count = 0;
 	char *token, *str_copy = strdup(str);
-
+	
+	if (str == NULL)
+		return (1);
 	if (str_copy == NULL)
 	{
 		if (str)
--- monty.h
+++ monty.h
@@ -85,5 +85,7 @@ void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 bool is_empty(void);
+
+
 extern info inf;
 #endif
